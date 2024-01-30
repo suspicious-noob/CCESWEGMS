fbaa44046026cccdf9fca36c3aa7ee3e17f49112
jeryymie07 <151909729+jeryymie07@users.noreply.github.com>
2024-01-30 13:57:30 +0800

    bootstrap and image
    image

---
+++ testpage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootsrap/css/bootstrap.min.css">
+    <title>Test Page</title>
+</head>
+
+<body>
+    <div class="card" style="width: 300px; margin: 50px;">
+        <div class="card-body">
+            <div class="form-floating">
+                <input class="form-control" type="text" name="username" id="username" placeholder="username">
+                <label for="username">Username</label>
+            </div>
+            <br>
+            <div class="form-floating">
+                <input class="form-control" type="text" name="Password" id="Password" placeholder="Password">
+                <label for="Password">Password</label>
+            </div>
+
+            <br>
+            <button class="btn btn-success" type="submit">Submit</button>
+        </div>
+
+    </div>
+</body>
+
+</html>
